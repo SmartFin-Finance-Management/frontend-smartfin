56685c650b2ec42323ee04f3c64449225dee6502
chetanananthu <chetanananthu@gmail.com>
2024-10-30 17:18:10 +0530

    added topbar

--- src/Pages/ProjectFormPage.tsx
+++ src/Pages/ProjectFormPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import TopBar from '../Components/TopBar';
 
 interface IProject {
     project_id: number;
@@ -93,114 +94,118 @@ export const ProjectFormPage: React.FC = () => {
     };
 
     return (
-        <div style={styles.container}>
-            <div style={styles.content}>
-                <div style={styles.titleContainer}>
-                    <h2 style={styles.title}>Project Information</h2>
+        <div >
+            <TopBar />
+            <div style={styles.container}>
+                <div style={styles.content}>
+                    <div style={styles.titleContainer}>
+                        <h2 style={styles.title}>Project Information</h2>
+                    </div>
+                    <form onSubmit={handleSubmit} style={styles.form}>
+                        <label style={styles.label}>
+                            Project ID:
+                            <input type="number" name="project_id" value={project.project_id} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            Organization ID:
+                            <input type="number" name="org_id" value={project.org_id} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            Client ID:
+                            <input type="number" name="client_id" value={project.client_id} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            Project Name:
+                            <input type="text" name="project_name" value={project.project_name} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            Start Date:
+                            <input type="date" name="start_date" value={project.start_date} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            End Date:
+                            <input type="date" name="end_date" value={project.end_date} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            Status:
+                            <select
+                                name="status"
+                                value={project.status}
+                                onChange={handleChange}
+                                style={styles.input}
+                                required
+                            >
+                                <option value="">Select Status</option>
+                                <option value="ongoing">onGoing</option>
+                                <option value="upcoming">upComing</option>
+                                <option value="completed">Completed</option>
+                            </select>
+                        </label>
+
+                        <label style={styles.label}>
+                            Total Budget:
+                            <input type="number" name="total_budget" value={project.total_budget} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            Allocated Budget:
+                            <input type="number" name="allocated_budget" value={project.allocated_budget} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            Remaining Budget:
+                            <input type="number" name="remaining_budget" value={project.remaining_budget} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            Employee Budget:
+                            <input type="number" name="employee_budget" value={project.employee_budget} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            Technical Budget:
+                            <input type="number" name="technical_budget" value={project.technical_budget} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            Additional Budget:
+                            <input type="number" name="additional_budget" value={project.additional_budget} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            Employee Expenses:
+                            <input type="number" name="employee_expenses" value={project.employee_expenses} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            Technical Expenses:
+                            <input type="number" name="technical_expenses" value={project.technical_expenses} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            Additional Expenses:
+                            <input type="number" name="additional_expenses" value={project.additional_expenses} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <label style={styles.label}>
+                            Actual Expenses:
+                            <input type="number" name="actual_expenses" value={project.actual_expenses} onChange={handleChange} style={styles.input} required />
+                        </label>
+
+                        <button type="submit" style={styles.button}>
+                            Submit
+                        </button>
+                    </form>
                 </div>
-                <form onSubmit={handleSubmit} style={styles.form}>
-                    <label style={styles.label}>
-                        Project ID:
-                        <input type="number" name="project_id" value={project.project_id} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        Organization ID:
-                        <input type="number" name="org_id" value={project.org_id} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        Client ID:
-                        <input type="number" name="client_id" value={project.client_id} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        Project Name:
-                        <input type="text" name="project_name" value={project.project_name} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        Start Date:
-                        <input type="date" name="start_date" value={project.start_date} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        End Date:
-                        <input type="date" name="end_date" value={project.end_date} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        Status:
-                        <select
-                            name="status"
-                            value={project.status}
-                            onChange={handleChange}
-                            style={styles.input}
-                            required
-                        >
-                            <option value="">Select Status</option>
-                            <option value="ongoing">onGoing</option>
-                            <option value="upcoming">upComing</option>
-                            <option value="completed">Completed</option>
-                        </select>
-                    </label>
-
-                    <label style={styles.label}>
-                        Total Budget:
-                        <input type="number" name="total_budget" value={project.total_budget} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        Allocated Budget:
-                        <input type="number" name="allocated_budget" value={project.allocated_budget} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        Remaining Budget:
-                        <input type="number" name="remaining_budget" value={project.remaining_budget} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        Employee Budget:
-                        <input type="number" name="employee_budget" value={project.employee_budget} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        Technical Budget:
-                        <input type="number" name="technical_budget" value={project.technical_budget} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        Additional Budget:
-                        <input type="number" name="additional_budget" value={project.additional_budget} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        Employee Expenses:
-                        <input type="number" name="employee_expenses" value={project.employee_expenses} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        Technical Expenses:
-                        <input type="number" name="technical_expenses" value={project.technical_expenses} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        Additional Expenses:
-                        <input type="number" name="additional_expenses" value={project.additional_expenses} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <label style={styles.label}>
-                        Actual Expenses:
-                        <input type="number" name="actual_expenses" value={project.actual_expenses} onChange={handleChange} style={styles.input} required />
-                    </label>
-
-                    <button type="submit" style={styles.button}>
-                        Submit
-                    </button>
-                </form>
             </div>
         </div>
+
     );
 };
 
